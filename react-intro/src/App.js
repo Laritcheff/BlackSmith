ee781ef3e339fd772890f1354bcf6263c2329786
Karax <Laritcheff@gmail.com>
2020-12-07 20:49:24 +0300

    1

---
+++ react-intro/src/App.js
@@ -0,0 +1,24 @@
+import 'Menu.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Жопа!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-intro/src/menu.js
@@ -0,0 +1,18 @@
+
+
+    function Menu() {
+  return (
+    <header class="Menu">        
+         <nav>    
+            <a href="#aboutme">Обо мне</a>
+            <a href="#gallery">Галерея</a>
+            <a href="#skills">Технические навыки</a>
+            <a href="#portfolio">Мои проекты</a>
+            <a href="#contacts">Контакты</a>
+            <a href="rezume.pdf" target="_blank">Резюме</a>        
+        </nav>
+    </header>
+  );
+}
+
+export default Menu;
